26370f0f8dbf617b648e581e5eb38c2b5d169fbe
Jesús Varela <jesusaugusto.008@gmail.com>
2025-01-18 13:26:59 -0400

    feat: add vote seeders and adjust Vote table

--- backend/src/seed/seed.module.ts
+++ backend/src/seed/seed.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { CountrySeed } from './country/seed';
 import { SeedService } from './seed.service';
 import { UserSeed } from './user/seed';
+import { VoteSeed } from './vote/seed';
 
 @Module({
   imports: [PrismaModule],
-  providers: [SeedService, CountrySeed, UserSeed],
+  providers: [SeedService, CountrySeed, UserSeed, VoteSeed],
   exports: [SeedService],
 })
 export class SeedModule {}
--- backend/src/seed/seed.service.ts
+++ backend/src/seed/seed.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CountrySeed } from './country/seed';
 import { UserSeed } from './user/seed';
+import { VoteSeed } from './vote/seed';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -8,10 +9,12 @@ export class SeedService {
   constructor(
     private readonly countrySeed: CountrySeed,
     private readonly userSeed: UserSeed,
+    private readonly voteSeed: VoteSeed,
     private readonly prisma: PrismaService,
   ) {}
 
   async deleteAll() {
+    await this.prisma.vote.deleteMany();
     await this.prisma.user.deleteMany();
     await this.prisma.country.deleteMany();
   }
@@ -19,5 +22,6 @@ export class SeedService {
   async seedAll() {
     await this.countrySeed.seed();
     await this.userSeed.seed();
+    await this.voteSeed.seed();
   }
 }

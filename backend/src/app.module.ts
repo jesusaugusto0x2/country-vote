0c8ecf1a56fe21f10ac3c9dc2f05fcbd6076f97d
Jesús Varela <jesusaugusto.008@gmail.com>
2025-01-19 10:29:59 -0400

    feat: add votes module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SeedModule } from './seed/seed.module';
 import { CountriesModule } from './countries/countries.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [PrismaModule, SeedModule, CountriesModule],
+  imports: [PrismaModule, SeedModule, CountriesModule, VotesModule],
   controllers: [],
   providers: [],
 })

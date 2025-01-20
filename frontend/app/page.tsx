a08893132927baeda46b35cbb079c7fb9a5f2ea4
Jesús Varela <jesusaugusto.008@gmail.com>
2025-01-19 22:46:31 -0400

    fix: await for search parameter changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,11 @@
 import { Navbar } from "@/components/Navbar";
 import { HomePage } from "@/containers";
 
-export default async function Home({
-  searchParams,
-}: {
-  readonly searchParams: { readonly search?: string };
+export default async function Home(props: {
+  readonly searchParams: Promise<{ readonly search?: string }>;
 }) {
+  const searchParams = await props.searchParams;
+
   return (
     <main>
       <header>
